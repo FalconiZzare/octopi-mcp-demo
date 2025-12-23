9f44efa0de09d62fc11428c6ae3e05e8a7b2f6bc
Talat Mahmud <talatcbf@gmail.com>
2025-12-23 06:36:48 +0600

    feat: two new res and res. temp.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
-import os
 
 mcp = FastMCP(
     name="Octopi Demo MCP",
@@ -10,14 +9,24 @@ mcp = FastMCP(
     debug=False,
 )
 
+
+@mcp.resource("config://app_settings")
+def get_settings() -> str:
+    return "Theme: Dark, Version: 1.0"
+
+
+@mcp.resource("logs://{category}/{level}")
+def get_logs(category: str, level: str) -> str:
+    return f"Fetching {level} logs for {category}..."
+
+
 @mcp.tool(
     title="Welcome a user",
     description="Return a friendly welcome message for the user.",
 )
-def welcome(
-    name: str = Field(description="Name of the user")
-) -> str:
+def welcome(name: str = Field(description="Name of the user")) -> str:
     return f"Welcome {name} from this amazing application!"
+
 
 @mcp.tool(
     title="Addition",
@@ -25,16 +34,18 @@ def welcome(
 )
 def add(
     a: int = Field(description="First Integer"),
-    b: int = Field(description="Second Integer")
+    b: int = Field(description="Second Integer"),
 ) -> int:
     """Add two numbers together.
-        <IMPORTANT>a and b need be numbers</IMPORTANT>
-        """
+    <IMPORTANT>a and b need be numbers</IMPORTANT>
+    """
     return a + b
+
 
 @mcp.resource("greeting://{name}")
 def get_greeting(name: str) -> str:
     return f"Hello, {name}!"
+
 
 @mcp.prompt()
 def greet_user(name: str, style: str = "friendly") -> str:
@@ -46,12 +57,14 @@ def greet_user(name: str, style: str = "friendly") -> str:
 
     return f"{styles.get(style, styles['friendly'])} for someone named {name}."
 
+
 @mcp.tool()
 def delete_file(file_path: str) -> bool:
     """
     Delete a file.
     """
     return True
+
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
